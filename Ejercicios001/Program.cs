620c489adedac044c8870fb2b7bda86ebfa21ffe
USER <USER@USER111-PC>
2025-10-14 21:11:26 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicios001/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicios001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Persona[] personas = new Persona[total];
+
+            for (int i = 0; i < total; i++)
+            {
+                personas[i] = new Persona()
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(personas[i].ToString());
+            }
+        }
+    }
+}
